8bdc651217a0df0db0d3f539fff85f9590a4ccf7
Matthew Scharley <matt.scharley@gmail.com>
2013-11-03 21:13:59 +1100

    Finalised working version of the Compass generator

--- _plugins/compass_generator.rb
+++ _plugins/compass_generator.rb
@@ -2,35 +2,82 @@
 require 'sass/plugin'
 require 'compass'
 require 'compass/commands'
+require 'fileutils'
 
 module Jekyll
-  class Compass < Jekyll::Generator
+  class CompassFile < StaticFile
+    def write(destination)
+      # Short-circuit to the inevitable
+      false
+    end
+  end
+
+  class Compass < Generator
     safe true
-    priority :normal
+    priority :high
 
     def generate(site)
-      input = File.join(site.config['source'], '_sass')
+      input = File.join(site.source, '_sass')
 
       return unless File.exist? input
+      puts
+
+      output = File.join(site.config['destination'], 'css')
+      if File.exist? output
+        puts "Deleting existing CSS content..."
+
+        site.static_files = site.static_files.select do |p|
+          if p.path.index(output)
+            File.delete p.path
+            false
+          else
+            true
+          end
+        end
+      end
 
       config = {
-          :project_path => site.config['source'],
+          :project_path => site.source,
           :http_path => '/',
           :sass_path => input,
-          :css_path => File.join(site.config['destination'], 'css'),
-          :images_path => File.join(site.config['source'], 'images'),
-          :javascripts_path => File.join(site.config['destination'], 'js'),
+          :css_path => output,
+          :images_path => File.join(site.source, 'images'),
+          :javascripts_path => File.join(site.source, 'js'),
           :line_comments => false,
           :environment => :production,
           :output_style => :compact,
-          :quiet => true,
+          #:quiet => true,
           :sass_options => {
               :unix_newlines => true,
           },
       }
 
       ::Compass.add_configuration(config, 'Jekyll::Compass')
-      ::Compass::Commands::UpdateProject.new(site.config['source'], config).execute
+      ::Compass.configuration.on_sprite_saved do |filename|
+        site.static_files << StaticFile.new(site, site.source, File.dirname(filename)[site.source.length..-1], File.basename(filename))
+      end
+      ::Compass.configuration.on_stylesheet_saved do |filename|
+        source = site.config['destination']
+        site.static_files << CompassFile.new(site, source, File.dirname(filename)[source.length..-1], File.basename(filename))
+      end
+
+      # Manually mangle the output directory to keep it in sync with what Compass expects and Jekyll produces
+      ::Compass.configuration.on_sprite_removed do |filename|
+        site.static_files = site.static_files.select do |p|
+          if p.path == filename
+            sprite_output p.destination(site.config['destination'])
+            File.delete sprite_output if File.exist? sprite_output
+            false
+          else
+            true
+          end
+        end
+      end
+
+      compiler = ::Compass::Commands::UpdateProject.new(site.config['source'], config).new_compiler_instance
+
+      compiler.run
+      nil
     end
   end
 end
